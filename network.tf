edd84fca28ac38970041d502294ef2cff05dacee
Nic Jackson <jackson.nic@gmail.com>
2017-09-26 16:56:51 +0100

    Updated module

--- network.tf
+++ network.tf
@@ -2,5 +2,16 @@ data "google_compute_zones" "available" {}
 
 data "google_compute_subnetwork" "default" {
   name   = "default"
-  region = "${var.region}"
+  region = "${var.gcp_region}"
+}
+
+resource "google_compute_firewall" "http" {
+  name          = "http"
+  network       = "default"
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "cidr" {
-  value = "${data.google_compute_subnetwork.default.ip_cidr_range}"
+output "alb_dns" {
+  value = "${google_compute_global_address.external-address.address}"
 }
 
-output "region" {
-  value = "${var.region}"
+output "cidr" {
+  value = "${data.google_compute_subnetwork.default.ip_cidr_range}"
 }
